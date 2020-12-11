094f3f8e98c25bab3ed9a04ab4611a4fa604eb65
Enier Ramos García <enier290188@gmail.com>
2020-12-10 23:07:05 -0500

    step: logout

--- src/main/java/com/tts/springbootoauth2/SpringBootOauth2Application.java
+++ src/main/java/com/tts/springbootoauth2/SpringBootOauth2Application.java
@@ -11,14 +11,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter {
 
-    @GetMapping("/user")
+    @RequestMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
@@ -33,6 +34,12 @@ public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter {
                 )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
                 )
                 .oauth2Login();
         // @formatter:on
